0b2d58f85457b181bc8aa2d1f9c991bf191366cd
fabocv <fabianleandro.cv@gmail.com>
2025-12-19 13:37:45 -0300

    orquestador concurrently en el proyecto

--- apps/controller-vanillajs/server.js
+++ apps/controller-vanillajs/server.js
@@ -8,7 +8,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 io.on('connection', (socket) => {
     socket.on('start-test', async (data) => {
